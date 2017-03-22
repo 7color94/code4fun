fa16e78043e4b6a86e4dfc99c8739bc0c71fb37c
7color94 <sukai94.gm@gmail.com>
2017-03-22 11:14:12 +0800

    leetcode 169 bit manipulation

---
+++ leetcode/leetcode169.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<time.h>
+#include<vector>
+
+using namespace std;
+
+#define random(x) (rand()%x)
+
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int major = 0, n = nums.size();
+		for (int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
+			int bitCounts = 0;
+			for (int j = 0; j < n; j++) {
+				if (nums[j] & mask) bitCounts++;
+				if (bitCounts > n / 2) {
+					major |= mask;
+					break;
+				}
+			}
+		}
+		return major;
+	}
+};
+
+int main() {
+	Solution s;
+	vector<int> nums{1,1,3,3,3};
+	int major = s.majorityElement(nums);
+	std::cout << major << std::endl;
+}
