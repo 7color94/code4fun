04b414244daf588fa291b2251ce583a4070790c3
7color94 <sukai94.gm@gmail.com>
2017-03-31 15:30:13 +0800

    hiho 1399 stack

---
+++ hihocoder/hiho1399.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<string>
+#include<fstream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int lens, cc = 0;
+	std::cin >> lens;
+	stack<int> s;
+	for (int i = 0; i < lens; i++) {
+		int num;
+		std::cin >> num;
+		if (!s.empty() && (s.top() + num) % 2 == 1) {
+			s.pop();
+			cc += 2;
+		}
+		else {
+			s.push(num);
+		}
+	}
+	std::cout << lens - cc << std::endl;
+}
