49070c0c7e968ecb2c428034be4ea813fec92bb8
7color94 <sukai94.gm@gmail.com>
2017-03-31 15:40:35 +0800

    hiho 1366 map<string, string>

---
+++ hihocoder/hiho1366.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<string>
+#include<fstream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int N;
+	std::cin >> N;
+	vector<string> strs;
+	string input;
+	while (N--) {
+		std::cin >> input;
+		strs.push_back(input);
+	}
+	map<string, string> mapstring;
+	int count = 0;
+	for (size_t i = 0; i < strs.size(); i++) {
+		if (mapstring.end() == mapstring.find(strs[i])) {
+			string rstrs(strs[i]);
+			reverse(rstrs.begin(), rstrs.end());
+			mapstring.insert(pair<string, string>(strs[i], rstrs));
+			mapstring.insert(pair<string, string>(rstrs, strs[i]));
+		}
+		else {
+			count++;
+		}
+	}
+	std::cout << count << std::endl;
+	//system("pause");
+	return 0;
+}
