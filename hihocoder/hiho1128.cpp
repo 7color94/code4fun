30fc517c36699ce479d101e0c808e88376e48308
7color94 <sukai94.gm@gmail.com>
2017-07-01 12:58:28 +0800

    快排 二分搜索

--- hihocoder/hiho1128.cpp
+++ hihocoder/hiho1128.cpp
@@ -1,55 +1,57 @@
-﻿#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<map>
-#include<string>
-#include<fstream>
-#include<stack>
-
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <queue>
 using namespace std;
 
-#define swap(x, t, y) (t = x, x = y, y = t)//交换x和y
+int N, key;
+int num[1000100];
 
-int a[1111111], k;
-
-int find(int s, int e)//快排思想
-{
-	if (s == e && a[s] != k)
-		return -1;
-	int i = s - 1, j, t, r, x = a[e];
-	for (j = s; j < e; j++)
-	{
-		if (a[j] <= x)
-		{
-			i++;
-			swap(a[i], t, a[j]);
-		}
-	}
-	swap(a[i + 1], t, a[e]);
-	if (a[i + 1] < k)
-	{
-		r = rand() % (e - i - 1) + i + 2;//随机选择a[r]作为主元
-		swap(a[r], t, a[e]);
-		find(i + 2, e);
-	}
-	else if (a[i + 1] > k)
-	{
-		r = rand() % (i + 1 - s) + s;
-		swap(a[r], t, a[i]);
-		find(s, i);
-	}
-	else
-		return i + 1;
+void swap(int ii, int jj) {
+    int tt = num[ii];
+    num[ii] = num[jj];
+    num[jj] = tt;
 }
 
-int main(void)
-{
-	int i, n, r, t;
-	cin >> n >> k;
-	for (i = 1; i <= n; i++)
-		cin >> a[i];
-	r = rand() % n + 1;
-	swap(a[r], t, a[n]);
-	cout << find(1, n) << endl;
-	//system("pause");
+int partition(int start, int end) {
+    int kk = num[end];
+    int i = start - 1;
+    for (int j = start; j < end; j++) {
+        if (num[j] < kk) swap(++i, j);
+    }
+    swap(++i, end);
+    return i;
+}
+
+int binary_search_key(int start, int end, int kk) {
+    if (start <= end) {
+        int pos = partition(start, end);
+        int cc = pos - start + 1;
+        if (kk < num[pos]) return binary_search_key(start, pos - 1, kk);
+        else if (kk > num[pos]) return binary_search_key(pos + 1, end, kk);
+        else return pos + 1;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    ifstream cin("in.txt");
+    
+    cin >> N >> key;
+    for (int i = 0; i < N; i++) {
+        cin >> num[i];
+    }
+
+    int ans = binary_search_key(0, N - 1, key);
+    cout << ans << endl;
+
+    return 0;
 }
