7247c4a6ad82a92bd42e44fcc2e9429fd1d85663
7color94 <sukai94.gm@gmail.com>
2017-08-02 15:50:17 +0800

    hiho 1322 N顶点M条边的图是树的冲要条件是N个顶点连通以及N=M+1

--- hihocoder/hiho1322.cpp
+++ hihocoder/hiho1322.cpp
@@ -9,6 +9,9 @@
 #include <string>
 #include <fstream>
 #include <queue>
+#include <ctime>
+#include <cstdlib>
+#include <unordered_map>
 using namespace std;
 
 struct edge {
@@ -16,11 +19,9 @@ struct edge {
     int next;
 } e[300100];
 
-int T;
-int N, M;
+int first[600], vis[600];
+int T, N, M;
 int edgenum;
-int first[600];
-int vis[600];
 
 void add_edge(int u, int v) {
     e[edgenum].u = u;
@@ -34,85 +35,44 @@ void add_edge(int u, int v) {
     first[v] = edgenum++;
 }
 
-bool dfs(int father, int u) {
-    vis[u] = 1;
-    bool ok = true;
-    for (int k = first[u]; k != -1; k = e[k].next) {
-        int v = e[k].v;
-        if (vis[v] == 1 && v != father) {
-            ok = false;
-            break;
-        }
-        if (vis[v] == 0) {
-            if(dfs(u, v) == false) {
-                ok = false;
-                break;
+bool is_connect() {
+    queue<int> q;
+    q.push(1);
+    vis[1] = 1;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int k = first[u]; k != -1; k = e[k].next) {
+            int v = e[k].v;
+            if (vis[v] == 0) {
+                q.push(v);
+                vis[v] = 1;
             }
         }
     }
-    return ok;
-}
-
-int color[600];
-int cc;
-
-void dfs_color(int u, int _cc) {
-    color[u] = _cc;
-    for (int k = first[u]; k != -1; k = e[k].next) {
-        int v = e[k].v;
-        if (color[v] != 0) continue;
-        dfs_color(v, _cc);
+    for (int u = 1; u <= N; u++) {
+        if (vis[u] == 0) return false;
     }
+    return true;
 }
 
 int main() {
-    ifstream cin("in.txt");
+    // ifstream cin("in.txt");
     // freopen("in.txt", "r", stdin);
-    
     cin >> T;
     while (T--) {
-        cin >> N >> M;
-
-        int a, b;
         edgenum = 0;
         memset(first, -1, sizeof(first));
+        int a, b;
+        cin >> N >> M;
         for (int i = 1; i <= M; i++) {
             cin >> a >> b;
             add_edge(a, b);
         }
-
         memset(vis, 0, sizeof(vis));
-        bool ok = true;
-        for (int u = 1; u <= N; u++) {
-            vis[u] = 1;
-            for (int k = first[u]; k != -1; k = e[k].next) {
-                int v = e[k].v;
-                if (vis[v] == 1) continue;
-                if (dfs(u, v) == false) {
-                    ok = false;
-                    break;
-                }
-            }
-            if (ok == false) break;
-        }
-
-        if (ok) {
-            // for union find
-            cc = 0;
-            memset(color, 0, sizeof(color));
-            for (int u = 1; u <= N; u++) {
-                if (color[u] == 0) color[u] = ++cc;
-                for (int k = first[u]; k != -1; k = e[k].next) {
-                    int v = e[k].v;
-                    if (color[v] != 0) continue;
-                    dfs_color(v, color[u]);
-                }
-            }
-        }
-
-        if (ok == true && cc == 1) cout << "YES" << endl;
+        bool ok = is_connect();
+        if (ok && N == M + 1) cout << "YES" << endl;
         else cout << "NO" << endl;
     }
-
     return 0;
 }
